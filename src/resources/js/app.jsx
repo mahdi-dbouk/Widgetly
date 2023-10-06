520d4c321f03516562ebf8a2da6ee7c8520d096c
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-06 11:45:01 +0300

    [Frontend] Connected Main with Redux Store

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -2,5 +2,11 @@ import './bootstrap';
 import React from "react";
 import  ReactDOM from "react-dom/client";
 import Main from './main';
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <Main />
+    </Provider>
+);
