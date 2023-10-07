210598a46791506472d3305fd6d84c477bbd1c5a
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-07 20:39:17 +0300

    [Frontend] Added Github Reducer to Redux Store

--- src/resources/js/redux/store.js
+++ src/resources/js/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import githubReducer from './features/github/githubSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    github: githubReducer
+  },
 })
