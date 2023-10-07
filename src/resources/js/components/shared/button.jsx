501375a61aa8b935db729fea32e049554892c5de
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-07 20:42:29 +0300

    [Frontend] Created a Simple Reusable Button Component

---
+++ src/resources/js/components/shared/button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({text, icon, style, action}) => {
+    return (
+      <div>
+          <button className={`${style}`} onClick={action}>
+              {icon}{text}
+          </button>
+      </div>
+    )
+  }
+  
+  export default Button;
