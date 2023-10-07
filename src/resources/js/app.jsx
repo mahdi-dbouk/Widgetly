632c90da54145deed44d4688d839cd5f7cce5799
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-07 20:40:42 +0300

    [Frontend] Imported CSS Classes for React-Grid-Layout Module

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -4,9 +4,11 @@ import  ReactDOM from "react-dom/client";
 import Main from './main';
 import store from './redux/store'
 import { Provider } from 'react-redux'
+import '/node_modules/react-grid-layout/css/styles.css'
+import '/node_modules/react-resizable/css/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <Main />
-    </Provider>
+   </Provider>
 );
