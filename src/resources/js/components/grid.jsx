b8b01a8db3fa1ae4d5f6d580c262686e66bd97b0
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-08 23:42:04 +0300

    [Frontend] Updated Grid Component

--- src/resources/js/components/grid.jsx
+++ src/resources/js/components/grid.jsx
@@ -5,10 +5,10 @@ import Widget from "./widget";
 const Grid = () => {
 
     const layout = [
-        { i: "a", x: 0, y: 0, w: 15, h: 10},
-        { i: "b", x: 16, y: 0, w: 15, h: 10},
-        { i: "c", x: 0, y: 11, w: 15, h: 10},
-        { i: "d", x: 16, y: 11, w: 15, h: 10}
+        { i: "a", x: 0, y: 0, w: 10, h: 5, minW: 5},
+        { i: "b", x: 11, y: 0, w: 10, h: 5},
+        { i: "c", x: 0, y: 6, w: 10, h: 5},
+        { i: "d", x: 11, y: 5, w: 10, h: 5}
       ];
       return <GridLayout
           className="layout"
@@ -17,8 +17,8 @@ const Grid = () => {
           rowHeight={30}
           width={1200}
         >
-          <div key="d" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"} type='commits'/></div>
-          <div key="a" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Languages Used"} type='langs'/></div>
+          <div key="d" className="border-2 border-gray-200 shadow-md p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"} type='commits' owner='tensorflow' repo='tensorflow'/></div>
+          <div key="a" className="border-2 border-gray-200 shadow-md p-5 rounded-md h-auto"><Widget title={"Languages Used"} type='langs' owner='tensorflow' repo='tensorflow'/></div>
 {/*           <div key="b" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"}/></div>
           <div key="c" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"}/></div> */}
 
