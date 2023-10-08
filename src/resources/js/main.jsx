32abf6b68d73e0558cc5657c3f36d38ad0e72dd1
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-08 23:38:02 +0300

    [Frontend] Implemented Modal State Controls in Main

--- src/resources/js/main.jsx
+++ src/resources/js/main.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "./components/grid";
 import Navbar from "./components/navbar";
 import WidgetControls from "./components/widgetControls";
+import CreateWidgetModal from "./components/createWidgetModal";
 
 const Main = () => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    useEffect(()=>{
+        console.log(modalIsOpen);
+    },[modalIsOpen])
     return <div>
         <Navbar />
-        <WidgetControls />
+        <WidgetControls setModalState={setIsOpen} />
+        <CreateWidgetModal modalState={modalIsOpen} setModalState={setIsOpen}/>
         <Grid />
     </div>
 }
