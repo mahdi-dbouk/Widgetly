912092e7bd69f3624148413f395a4358a3ae5505
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-08 23:39:27 +0300

    [Frontend] Updated Styling for Navbar

--- src/resources/js/components/navbar.jsx
+++ src/resources/js/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-    return <div className="h-12 w-auto flex flex-row justify-start items-center px-5 border-b-2 gap-1 border-green-500">
+    return <div className="h-12 w-auto flex flex-row justify-start items-center px-5 gap-1 shadow-md">
         <div className="bg-green-500 rounded-full h-4 w-4"></div>
         <span className="font-bold text-xl">Widgetly</span>
     </div>
