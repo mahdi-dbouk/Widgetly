cbf4f916081236f0546171fe784b456048446c3d
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-08 23:36:21 +0300

    [Frontend] Set App Element for Modal

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -6,6 +6,9 @@ import store from './redux/store'
 import { Provider } from 'react-redux'
 import '/node_modules/react-grid-layout/css/styles.css'
 import '/node_modules/react-resizable/css/styles.css'
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
