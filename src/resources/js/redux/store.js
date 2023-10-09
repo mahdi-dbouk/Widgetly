92844d84615f5e0174c39b3c40cde865b5ce07b1
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-09 12:38:25 +0300

    [Frontend] Added Widget Reducer to Redux Store

--- src/resources/js/redux/store.js
+++ src/resources/js/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import githubReducer from './features/github/githubSlice';
+import widgetReducer from './features/widget/widgetSlice';
 
 export default configureStore({
   reducer: {
-    github: githubReducer
+    github: githubReducer,
+    widget: widgetReducer
   },
 })
