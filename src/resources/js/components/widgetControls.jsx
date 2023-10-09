db820594dc0e40565bf204db94ec33ce713c9039
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-09 12:38:02 +0300

    [Frontend] Updated Widget Controls Component

--- src/resources/js/components/widgetControls.jsx
+++ src/resources/js/components/widgetControls.jsx
@@ -10,8 +10,8 @@ const WidgetControls = ({setModalState}) => {
     return <div className="h-14 flex flex-row px-5 justify-start items-center">
         <Button
             text={"New Widget"} 
-            icon={<FontAwesomeIcon icon={faAdd} color="white" scale={2} />}
-            style={'bg-green-500 p-2 rounded-full text-sm font-bold text-white'}
+            icon={<FontAwesomeIcon icon={faAdd} color="white" scale={1} />}
+            style={'bg-green-500 p-2 rounded-full text-xs font-bold text-white'}
             action={openModal}
         />
     </div>
