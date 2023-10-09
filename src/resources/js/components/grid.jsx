0c6b439cde38bf19cb7e964c00ab83f367788e46
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-10-09 12:37:02 +0300

    [Frontend] Implemented Logic to Spawn Widgets Dynamically on Grid

--- src/resources/js/components/grid.jsx
+++ src/resources/js/components/grid.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GridLayout from "react-grid-layout";
 import Widget from "./widget";
+import { useSelector } from "react-redux";
 
 const Grid = () => {
 
+  const widgetData = useSelector((state) => state.widget);
+  const [widgetsList, setWidgetsList] = useState([])
+
+  useEffect(()=>{
+    if(widgetData.title)
+      setWidgetsList([...widgetsList, widgetData])
+  },[widgetData])
+
+
     const layout = [
-        { i: "a", x: 0, y: 0, w: 10, h: 5, minW: 5},
-        { i: "b", x: 11, y: 0, w: 10, h: 5},
-        { i: "c", x: 0, y: 6, w: 10, h: 5},
-        { i: "d", x: 11, y: 5, w: 10, h: 5}
+        {i: 'a', x: 0, y: 0, w: 10, h: 5, minW: 5},
+        {i: 'b', x: 11, y: 0, w: 10, h: 5},
+        {i: 'c', x: 0, y: 6, w: 10, h: 5},
+        {i: 'd', x: 11, y: 6, w: 10, h: 5},
+        {i: 'e', x: 0, y: 11, w: 10, h: 5}
       ];
       return <GridLayout
           className="layout"
@@ -17,10 +28,23 @@ const Grid = () => {
           rowHeight={30}
           width={1200}
         >
-          <div key="d" className="border-2 border-gray-200 shadow-md p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"} type='commits' owner='tensorflow' repo='tensorflow'/></div>
-          <div key="a" className="border-2 border-gray-200 shadow-md p-5 rounded-md h-auto"><Widget title={"Languages Used"} type='langs' owner='tensorflow' repo='tensorflow'/></div>
-{/*           <div key="b" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"}/></div>
-          <div key="c" className="border-2 border-blue-600 p-5 rounded-md h-auto"><Widget title={"Github Commits Per Date"}/></div> */}
+          {widgetsList.map((widget, index) => (
+                  <div
+                    key={index}
+                    className="border-2 border-gray-200 shadow-md p-5 rounded-md"
+                    data-grid={layout[index]}
+                  >
+                    <Widget
+                      title={widget.title}
+                      description={widget.description}
+                      about={widget.about}
+                      type={widget.chartType}
+                      owner={widget.owner}
+                      repo={widget.repo}
+                      colors={widget.colors}
+                    />
+                  </div>
+                ))}
 
         </GridLayout> 
 }
